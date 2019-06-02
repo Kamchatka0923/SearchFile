a6cc20184de65531c469191dd495f830fed5e609
小宠物 <@DESKTOP-IR2PMM7>
2019-06-02 11:12:24 +0800

    201906021112

---
+++ SearchFiles/FormSearchFiles.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SearchFiles
+{
+    partial class FormSearchFiles
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSearchFiles));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(569, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始搜索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "关键字";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(160, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(594, 280);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(651, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "包括文件夹";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(160, 325);
+            this.treeView1.TabIndex = 8;
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // FormSearchFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 325);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "FormSearchFiles";
+            this.Text = "查找文件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ SearchFiles/FormSearchFiles.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace SearchFiles
+{
+    public partial class FormSearchFiles : Form
+    {
+        public FormSearchFiles()
+        {
+            InitializeComponent();
+        }
+        string path = string.Empty;
+        string keyword = string.Empty;
+        Queue<string> FileNames = new Queue<string>();
+        void SearchFile(object path)
+        {
+            try
+            {
+                DirectoryInfo DI = new DirectoryInfo(path.ToString());
+                FileInfo[] FIs = DI.GetFiles("*" + keyword + "*");
+                DirectoryInfo[] FNs = DI.GetDirectories("*" + keyword + "*");
+                DirectoryInfo[] DIs = DI.GetDirectories();
+                lock (FileNames)
+                {
+                    foreach (var it in FIs)
+                    {
+                        FileNames.Enqueue(it.FullName);
+                        SetListbox(it.FullName);
+                    }
+                    if (checkBox1.Checked)
+                        foreach (var it in FNs)
+                        {
+                            FileNames.Enqueue(it.FullName);
+                            SetListbox(it.FullName);
+                        }
+                }
+                foreach (var it in DIs)
+                {
+                    Thread thread = new Thread(new ParameterizedThreadStart(SearchFile));
+                    thread.Start(it.FullName);
+                }
+            }
+            catch { }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            FileNames.Clear();
+            keyword = textBox1.Text;
+            if (path == "计算机")
+                foreach (var it in Directory.GetLogicalDrives())
+                {
+                    Thread thread = new Thread(new ParameterizedThreadStart(SearchFile));
+                    thread.Start(it);
+                }
+            SearchFile(path);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this,new EventArgs());
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int index = listBox1.IndexFromPoint(((MouseEventArgs)e).Location);
+            if(index!=ListBox.NoMatches)
+            {
+                try
+                {
+                    string filepath = listBox1.Items[listBox1.SelectedIndex].ToString();
+                    if(File.Exists(filepath))
+                        filepath = filepath.Remove(filepath.LastIndexOf(@"\"));
+                    System.Diagnostics.Process.Start("explorer.exe", @filepath);
+                }
+                catch { MessageBox.Show("未选择要打开的文件路径"); }
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            TreeNode computer = treeView1.Nodes.Add("计算机");
+            foreach (var it in Directory.GetLogicalDrives())
+                computer.Nodes.Add(it);
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            foreach(TreeNode node in e.Node.Nodes)
+            {
+                if(node.Nodes.Count==0)
+                {
+                    string nodepath = node.FullPath.Remove(0, 4);
+                    DirectoryInfo dir = new DirectoryInfo(nodepath);
+                    try
+                    {
+                        foreach (var it in dir.GetDirectories())
+                        {
+                            if (it.Name.Contains(@"$") || it.Name.Contains("System Volume Information"))
+                                continue;
+                            node.Nodes.Add(it.Name);
+                        }
+                    }
+                    catch { continue; }
+                }
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                path = e.Node.FullPath.Remove(0, 4);
+                this.Text = "查找文件：" + path;
+            }
+            catch
+            {
+                path = e.Node.FullPath;
+                this.Text = "查找文件：" + path;
+            }
+        }
+
+        private delegate void SetListboxDelegate(string text);
+        private void SetListbox(string text)
+        {
+            if(listBox1.InvokeRequired)
+            {
+                SetListboxDelegate d = new SetListboxDelegate(SetListbox);
+                this.Invoke(d, text);
+            }
+            else
+            {
+                listBox1.Items.Add(text);
+            }
+        }
+    }
+}
